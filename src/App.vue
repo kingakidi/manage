e426127b0974d2e5ce8308ec5faa9c6f843fd073
kingakidi <53876486+kingakidi@users.noreply.github.com>
2022-04-27 21:26:14 +0100

    before first shut down

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ export default {
   name: "App",
   components: {
     PostForm,
-  },
+  },cd 
 };
 </script>
 
--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -1,9 +1,33 @@
 <template lang="">
   <div>
     <h2>Post Form</h2>
+    <form @submit.prevent="postData">
+      <input type="text" name="author" v-model="posts.author" /> <br />
+      <input type="text" name="title" v-model="posts.title" /> <br />
+      <button>Submit</button>
+    </form>
   </div>
 </template>
 <script>
-export default {};
+import  Vue from "vue"; // in Vue 3
+import axios from "axios";
+import VueAxios from "vue-axios";
+export default {
+  data() {
+    return {
+      posts: {
+        title: null,
+        author: null,
+      },
+    };
+  },
+
+  methods: {
+    postData(e) {
+      console.log(e);
+      
+    },
+  },
+};
 </script>
 <style lang=""></style>
